d100f8d9bcd4687c1dcef7069f228d4a33b84d85
R35 <R35@DESKTOP-O3I6T17>
2020-03-25 14:24:03 +0900

    Web Config #3

--- mysite05/src/main/java/com/douzone/mysite/controller/UserController.java
+++ mysite05/src/main/java/com/douzone/mysite/controller/UserController.java
@@ -73,7 +73,15 @@ public class UserController {
 		return "redirect:/";
 	}
 	
-
+	@RequestMapping(value="/auth", method=RequestMethod.POST)
+	public void auth() {
+		
+	}
+	
+	@RequestMapping(value="/logout", method=RequestMethod.GET)
+	public void logout() {
+		
+	}
 
 	
 }
