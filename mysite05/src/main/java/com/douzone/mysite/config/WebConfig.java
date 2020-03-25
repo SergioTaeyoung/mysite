4ad38e27cb96f8e9910fa723b57cebc87ca91d47
R35 <R35@DESKTOP-O3I6T17>
2020-03-25 15:14:27 +0900

    property 설정 & message source

--- mysite05/src/main/java/com/douzone/mysite/config/WebConfig.java
+++ mysite05/src/main/java/com/douzone/mysite/config/WebConfig.java
@@ -5,13 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import com.douzone.mysite.config.web.MessageConfig;
 import com.douzone.mysite.config.web.MvcConfig;
 import com.douzone.mysite.config.web.SecurityConfig;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan({"com.douzone.mysite.controller, com.douzone.mysite.exception"})
-@Import({MvcConfig.class, SecurityConfig.class})
+@Import({MvcConfig.class, SecurityConfig.class, MessageConfig.class})
 public class WebConfig {
 
 }
