eebe39ca43f0db3b81fa41176ba062a7ba451f54
R35 <R35@DESKTOP-O3I6T17>
2020-03-25 15:39:04 +0900

    MessageConfig & fileupload

--- mysite05/src/main/java/com/douzone/mysite/config/WebConfig.java
+++ mysite05/src/main/java/com/douzone/mysite/config/WebConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import com.douzone.mysite.config.web.FileUploadConfig;
 import com.douzone.mysite.config.web.MessageConfig;
 import com.douzone.mysite.config.web.MvcConfig;
 import com.douzone.mysite.config.web.SecurityConfig;
@@ -12,7 +13,7 @@ import com.douzone.mysite.config.web.SecurityConfig;
 @Configuration
 @EnableWebMvc
 @ComponentScan({"com.douzone.mysite.controller, com.douzone.mysite.exception"})
-@Import({MvcConfig.class, SecurityConfig.class, MessageConfig.class})
+@Import({MvcConfig.class, SecurityConfig.class, MessageConfig.class, FileUploadConfig.class})
 public class WebConfig {
 
 }
