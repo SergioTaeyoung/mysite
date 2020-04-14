0f7f8c87db22f6270668fa1d59ac079c251172e7
R35 <xodud7965@naver.com>
2020-04-14 15:48:15 +0900

    application.yml

--- mysite07/src/main/java/com/douzone/mysite/BootApp.java
+++ mysite07/src/main/java/com/douzone/mysite/BootApp.java
@@ -3,7 +3,9 @@ package com.douzone.mysite;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+
 public class BootApp {
 	public static void main(String[] args) {
 		SpringApplication.run(BootApp.class, args);
